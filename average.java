4fc9a087151de13ace94a70701416bd523017cee
divya <gutamdivya70939@gmail.com>
2021-08-27 08:47:09 +0530

    first commit

---
+++ average.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ simpleinterest.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class simpleinterest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
